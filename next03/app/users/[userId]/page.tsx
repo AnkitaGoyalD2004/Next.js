98c47e09e19604401ea4e3003436477f7f3aa795
Ankita Goyal <goyalankita0325@gmail.com>
2025-01-15 17:21:21 +0530

    about fetching data

--- next03/app/users/[userId]/page.tsx
+++ next03/app/users/[userId]/page.tsx
@@ -1,11 +1,33 @@
+import getUser from "@/lib/getUser";
+import getUserPosts from "@/lib/getUserPosts";
+import { Suspense } from "react";
+import UserPosts from "./components/UserPosts";
+
 type Params = {
-    params : {
-        userId : string
-    }
+  params: {
+    userId: string;
+  };
+};
+
+export default async function UserPage({ params: { userId } }: Params) {
+  // Fetch user and posts concurrently
+  const userData: Promise<User> = getUser(userId);
+  const userPostsData: Promise<Post[]> = getUserPosts(userId);
+
+  // // Await both promises
+  // const [user, userPosts] = await Promise.all([userData, userPostsData]);
+
+   const user = await  userData
+
+
+  return (
+    <>
+      <h2>{user.name}</h2>
+      <br />
+      <Suspense fallback={<h2>Loading...</h2>}>
+      <UserPosts promise={userPostsData} />
+      </Suspense>
+    </>
+  );
 }
 
-export default function UserPage ({params : {userId}} : Params)  {
-  return (
-    <div>page</div>
-  )
-}
--- next03/lib/getUserPosts.tsx
+++ next03/lib/getUserPosts.tsx
@@ -1,5 +1,5 @@
 export default async function getUser(userId : string) {
-    const res = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts?userId = ${userId}`)
 
     if(!res.ok) throw new Error('failed to fetch user')
 
