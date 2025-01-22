4329ec36d508ab528d4881eafd35a0db51edc8c2
Ankita Goyal <goyalankita0325@gmail.com>
2025-01-22 19:34:51 +0530

    about SSG , SSR , ISR

--- next04/app/users/[userId]/page.tsx
+++ next04/app/users/[userId]/page.tsx
@@ -1,43 +1,62 @@
-import getUser from "@/lib/getUser";
-import getUserPosts from "@/lib/getUserPosts";
-import { Metadata } from "next";
-import { Suspense } from "react";
-import UserPosts from "./components/UserPosts";
+import getAllUsers from "@/lib/getAllUsers"
+import getUser from "@/lib/getUser"
+import getUserPosts from "@/lib/getUserPosts"
+import type { Metadata } from 'next'
+import { Suspense } from "react"
+import UserPosts from "./components/UserPosts"
+
+import { notFound } from 'next/navigation'
 
 type Params = {
-  params: {
-    userId: string;
-  };
-};
+    params: {
+        userId: string
+    }
+}
 
-export async function generateMetadata({params : {userId}}:Params):Promise<Metadata>{
-  const userData: Promise<User> = getUser(userId);
-  const user : User = await userData
-  return {
-    title: user.name
-    // description: `This is the page of ${user.name}`
-  }
+export async function generateMetadata({ params: { userId } }: Params): Promise<Metadata> {
+    const userData: Promise<User> = getUser(userId)
+    const user: User = await userData
+
+    if (!user.name) {
+        return {
+            title: "User Not Found"
+        }
+    }
+
+    return {
+        title: user.name,
+        description: `This is the page of ${user.name}`
+    }
+
 }
 
 export default async function UserPage({ params: { userId } }: Params) {
-  // Fetch user and posts concurrently
-  const userData: Promise<User> = getUser(userId);
-  const userPostsData: Promise<Post[]> = getUserPosts(userId);
+    const userData: Promise<User> = getUser(userId)
+    const userPostsData: Promise<Post[]> = getUserPosts(userId)
 
-  // // Await both promises
-  // const [user, userPosts] = await Promise.all([userData, userPostsData]);
+    //const [user, userPosts] = await Promise.all([userData, userPostsData])
 
-   const user = await  userData
+    const user = await userData
 
+    if (!user.name) notFound()
 
-  return (
-    <>
-      <h2>{user.name}</h2>
-      <br />
-      <Suspense fallback={<h2>Loading...</h2>}>
-      <UserPosts promise={userPostsData} />
-      </Suspense>
-    </>
-  );
+    return (
+        <>
+            <h2>{user.name}</h2>
+            <br />
+            <Suspense fallback={<h2>Loading...</h2>}>
+                {/* @ts-expect-error Server Component */}
+                <UserPosts promise={userPostsData} />
+            </Suspense>
+        </>
+    )
 }
 
+export async function generateStaticParams() {
+    const usersData: Promise<User[]> = getAllUsers()
+    const users = await usersData
+
+    return users.map(user => ({
+        userId: user.id.toString()
+    }))
+}
